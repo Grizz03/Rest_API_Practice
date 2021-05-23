2c7a8914ca060ebf90bc4d5ffa15ba210caa85b7
Grizz03 <steve.grizz03@gmail.com>
2021-05-23 01:22:23 -0700

    initiated store routes

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+# POST /store data: {name:}
+@app.route('/store', methods=['POST'])
+def create_store(name):
+    pass
+
+
+# GET /store/<string:name>
+@app.route('/store/<string:name>', methods=['GET'])
+def get_store(name):
+    pass
+
+
+# GET /store
+@app.route('/store')
+def get_store():
+    pass
+
+
+# POST /store/<string:name>/item {name:, price:}
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_store(name, price):
+    pass
+
+
+# GET /store/<string:name>/item
+@app.route('/store/<string:name>/item', methods=['GET'])
+def get_store(name):
+    pass
+
+
+app.run(port=5000)
